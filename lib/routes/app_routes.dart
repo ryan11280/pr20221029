f5d256de8e5dc1d5e16b6a04784f59b8261ed527
erinri89 <jean890322@gmail.com>
2022-11-02 15:17:21 +0800

    1102 3.

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -1,10 +1,12 @@
 import '../screens/splash_screen.dart';
 import 'package:get/get.dart';
+import '../screens/welcome/welcome_screen.dart';
+
 
 class AppRoutes {
   static List<GetPage> routes() =>
       [
-        GetPage(name: "/", page: () => SplashScreen()) //預設首頁
+        GetPage(name: "/", page: () => WelcomeScreen()) //預設首頁
         //GetPage(name: "/introduction", page: () => SplashScreen())
 
       ];

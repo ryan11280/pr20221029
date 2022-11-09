c3ce59995454c02c676c56806e50708a0c2f2bf2
ryan <ryan11280@gmail.com>
2022-11-09 14:01:33 +0800

    1108night+ 題目製作頁layout基本

--- lib/screens/create/create_question.dart
+++ lib/screens/create/create_question.dart
@@ -6,6 +6,11 @@
 
 import 'package:flutter/material.dart';
 
+import '../../configs/themes/app_colors.dart';
+import 'add_question_book_popup.dart';
+import 'package:get/get.dart';
+import'package:pr20221029/screens/home/home1_screen.dart';
+
 
 class radioTest extends StatelessWidget {
   const radioTest({super.key});
@@ -32,43 +37,138 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
 
   @override
   Widget build(BuildContext context) {
+    Future.delayed(Duration.zero, () => popupDialogBuilder(context)); //先彈窗 https://stackoverflow.com/questions/52164369/show-alert-dialog-on-app-main-screen-load-automatically-in-flutter
     return Scaffold(
-      body: SafeArea(
-        child: Column(
-          children: <Widget>[
-            ListTile(
-              title: const Text('Lafayette'),
-              leading: Radio<SingingCharacter>(
-                value: SingingCharacter.lafayette,
-                groupValue: _character,
-                onChanged: (SingingCharacter? value) {
-                  setState(() {
-                    _character = value;
-                  });
-                },
+      appBar: AppBar(
+        backgroundColor: kGrayColor,
+        title: const Text(
+          '題目製作頁',
+          style: TextStyle(fontWeight: FontWeight.w200),
+
+        ),
+
+
+        actions: <Widget>[
+
+          Padding(
+            padding: const EdgeInsets.only(right:25.0),
+            child: IconButton(
+              icon: Icon(
+                  Icons.home,
+                  color: Colors.white,
+                  size:30.0
               ),
+              onPressed: () {
+                Get.offAll(()=>Home());
+                // do something
+              },
             ),
-            ListTile(
-              title: const TextField(
-                maxLines:1,
-                minLines:1,
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: Colors.white,
-                  hintText: '請輸入題目',
+          )
+        ],
+      ),
+      body: Material(
+        child: SafeArea(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                //上區
+                Container(
+                  child: Text("show questions here.", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30)),
                 ),
-              ),
-              leading: Radio<SingingCharacter>(
-                value: SingingCharacter.jefferson,
-                groupValue: _character,
-                onChanged: (SingingCharacter? value) {
-                  setState(() {
-                    _character = value;
-                  });
-                },
-              ),
+                SizedBox(
+                  height: 30,
+                ),
+                //下區
+                Container(
+                  child: Column(
+                    children: [
+                      ListTile(
+                        title: const TextField(
+                          maxLines:1,
+                          minLines:1,
+                          decoration: InputDecoration(
+                            filled: true,
+                            fillColor: Colors.white,
+                            hintText: '請輸入選項一',
+                          ),
+                        ),
+                        leading: Radio<SingingCharacter>(
+                          value: SingingCharacter.jefferson,
+                          groupValue: _character,
+                          onChanged: (SingingCharacter? value) {
+                            setState(() {
+                              _character = value;
+                            });
+                          },
+                        ),
+                      ),
+                      ListTile(
+                        title: const TextField(
+                          maxLines:1,
+                          minLines:1,
+                          decoration: InputDecoration(
+                            filled: true,
+                            fillColor: Colors.white,
+                            hintText: '請輸入選項二',
+                          ),
+                        ),
+                        leading: Radio<SingingCharacter>(
+                          value: SingingCharacter.jefferson,
+                          groupValue: _character,
+                          onChanged: (SingingCharacter? value) {
+                            setState(() {
+                              _character = value;
+                            });
+                          },
+                        ),
+                      ),
+                      ListTile(
+                        title: const TextField(
+                          maxLines:1,
+                          minLines:1,
+                          decoration: InputDecoration(
+                            filled: true,
+                            fillColor: Colors.white,
+                            hintText: '請輸入選項三',
+                          ),
+                        ),
+                        leading: Radio<SingingCharacter>(
+                          value: SingingCharacter.jefferson,
+                          groupValue: _character,
+                          onChanged: (SingingCharacter? value) {
+                            setState(() {
+                              _character = value;
+                            });
+                          },
+                        ),
+                      ),
+                      ListTile(
+                        title: const TextField(
+                          maxLines:1,
+                          minLines:1,
+                          decoration: InputDecoration(
+                            filled: true,
+                            fillColor: Colors.white,
+                            hintText: '請輸入選項四',
+                          ),
+                        ),
+                        leading: Radio<SingingCharacter>(
+                          value: SingingCharacter.jefferson,
+                          groupValue: _character,
+                          onChanged: (SingingCharacter? value) {
+                            setState(() {
+                              _character = value;
+                            });
+                          },
+                        ),
+                      )
+                    ],
+                  ),
+                )
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );

178d9d682f47ff0371d80f47a0966ed650147f3b
ryan <ryan11280@gmail.com>
2022-11-09 14:00:24 +0800

    1108night 測試popup

---
+++ lib/screens/create/add_question_book_popup.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:xen_popup_card/xen_card.dart';
+
+//showdialog
+Future<void> popupDialogBuilder(BuildContext context) {
+  return showDialog<void>(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: const Text('Basic dialog title'),
+        content: const Text('A dialog is a type of modal window that\n'
+            'appears in front of app content to\n'
+            'provide critical information, or prompt\n'
+            'for a decision to be made.'),
+        actions: <Widget>[
+          TextButton(
+            style: TextButton.styleFrom(
+              textStyle: Theme.of(context).textTheme.labelLarge,
+            ),
+            child: const Text('Disable'),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+          TextButton(
+            style: TextButton.styleFrom(
+              textStyle: Theme.of(context).textTheme.labelLarge,
+            ),
+            child: const Text('Enable'),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+        ],
+      );
+    },
+  );
+}

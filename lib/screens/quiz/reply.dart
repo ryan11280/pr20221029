bf99101c51c5ac7680a12bdeffb02aaf070114cb
erinri89 <jean890322@gmail.com>
2022-11-21 12:33:03 +0800

    1108.4pm

--- lib/screens/quiz/reply.dart
+++ lib/screens/quiz/reply.dart
@@ -6,7 +6,6 @@ import '../create/create_question.dart';
 import '../home/home1_screen.dart';
 import 'components/body.dart';
 
-
 class reply extends StatefulWidget {
   const reply({Key? key}) : super(key: key);
 
@@ -18,33 +17,22 @@ class _replyState extends State<reply> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
-        appBar: AppBar(
-          title: const Text('回答問題'),
-          backgroundColor: kGrayColor,
-
-
-          actions: <Widget>[
-
-            Padding(
-              padding: const EdgeInsets.only(right:25.0),
-              child: IconButton(
-                icon: Icon(
-                    Icons.home,
-                    color: Colors.white,
-                    size:30.0
-                ),
-                onPressed: () {
-                  Get.offAll(()=>Home());
-                  // do something
-                },
-              ),
-            )
-          ],
-        ),
-
-
-
+      appBar: AppBar(
+        title: const Text('回答問題'),
+        backgroundColor: kGrayColor,
+        actions: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(right: 25.0),
+            child: IconButton(
+              icon: Icon(Icons.home, color: Colors.white, size: 30.0),
+              onPressed: () {
+                Get.offAll(() => Home());
+                // do something
+              },
+            ),
+          )
+        ],
+      ),
       body: Material(
         child: SafeArea(
           child: Center(
@@ -56,72 +44,83 @@ class _replyState extends State<reply> {
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     SizedBox(
-                      height: 50,
+                      height: 30,
                     ),
                     const Padding(
                       padding:
-                      EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                          EdgeInsets.symmetric(horizontal: 20, vertical: 10),
                       child: TextField(
                         maxLines: 12,
-                        minLines: 8,
+                        minLines: 7,
                         decoration: InputDecoration(
                           filled: true,
                           border: OutlineInputBorder(),
-
                         ),
                       ),
                     ),
-                    Container(),
                   ],
                 ),
 
                 SizedBox(
-                  height: 30,
+                  height: 20,
                 ),
-
 
                 //中間選擇區
+                //第一格
                 Container(
-                  height: 60,
-                  width: 430,
-
-                  child:
-                  Card(child: Text('11111')),
-
+                  height: 70,
+                  width: 350,
+                  child: Card(
+                      shape: RoundedRectangleBorder(
+                        side: BorderSide(color: Colors.black12, width: 1),
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                      child: Text('   A')),
                 ),
                 SizedBox(
                   height: 30,
                 ),
+//第二格
                 Container(
-                  height: 60,
-                  width: 430,
-                  child:
-                  const Card(child: Text('11111')
-                  ),
-
+                    height: 70,
+                    width: 350,
+                    child: Card(
+                        shape: RoundedRectangleBorder(
+                          side: BorderSide(color: Colors.black12, width: 1),
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        child: Text('   B'))),
+                SizedBox(
+                  height: 30,
+                ),
+                //第三格
+                Container(
+                  height: 70,
+                  width: 350,
+                  child: Card(
+                      shape: RoundedRectangleBorder(
+                        side: BorderSide(color: Colors.black12, width: 1),
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                      child: Text('   C')),
                 ),
                 SizedBox(
                   height: 30,
                 ),
+                //第四格
                 Container(
-                  height: 60,
-                  width: 430,
-                  child:
-                  Card(child: Text('11111')),
+                  height: 70,
+                  width: 350,
+                  child: Card(
+                      shape: RoundedRectangleBorder(
+                        side: BorderSide(color: Colors.black12, width: 1),
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                      child: Text('   D')),
 
                 ),
                 SizedBox(
-                  height: 30,
-                ),
-                Container(
-                  height: 60,
-                  width: 430,
-                  child:
-                  Card(child: Text('11111')),
-
-                ),
-                SizedBox(
-                  height: 30,
+                  height: 20,
                 ),
 
                 //下面按鈕
@@ -131,25 +130,31 @@ class _replyState extends State<reply> {
                     crossAxisAlignment: CrossAxisAlignment.end,
                     children: [
                       OutlinedButton(
+
                         child: Text('下一題'),
                         onPressed: () {},
+
                         style: OutlinedButton.styleFrom(
                           shadowColor: Colors.black87,
+                          fixedSize: Size(80, 30),
                           primary: kBlackColor,
                           shape: RoundedRectangleBorder(
                             borderRadius: BorderRadius.circular(18.0),
                           ),
                           side: BorderSide(width: 2, color: Colors.grey),
+
                         ),
                       ),
                       SizedBox(
-                        width: 15, // <-- SEE HERE
+                        width: 35, // <-- SEE HERE
                       ),
                       OutlinedButton(
-                        child: Text('完成'),
+                        child: Text('交卷'),
+
                         onPressed: () {},
                         style: OutlinedButton.styleFrom(
                           shadowColor: Colors.black87,
+                          fixedSize: Size(80, 30),
                           primary: kBlackColor,
                           shape: RoundedRectangleBorder(
                             borderRadius: BorderRadius.circular(18.0),
@@ -157,6 +162,8 @@ class _replyState extends State<reply> {
                           side: BorderSide(width: 2, color: Colors.grey),
                         ),
                       ),
+
+
                     ],
                   ),
                 ),
@@ -165,28 +172,6 @@ class _replyState extends State<reply> {
           ),
         ),
       ),
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     );
   }
 }

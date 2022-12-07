90878c202c995e798347047a51a87aab6f2ef772
erinri89 <jean890322@gmail.com>
2022-12-07 12:18:21 +0800

    1206.5pm

--- lib/screens/about/about_screen.dart
+++ lib/screens/about/about_screen.dart
@@ -1,7 +1,7 @@
 import 'package:footer/footer.dart';
 import 'package:footer/footer_view.dart';
 import 'package:flutter/material.dart';
-import'package:pr20221029/configs/themes/app_colors.dart';
+import 'package:pr20221029/configs/themes/app_colors.dart';
 import 'package:get/get.dart';
 import 'package:pr20221029/screens/home/home1_screen.dart';
 
@@ -57,22 +57,14 @@ class FooterPageState extends State<FooterPage> {
         title: const Text(
           '關於我們',
           style: TextStyle(fontWeight: FontWeight.w200),
-
         ),
-
-
         actions: <Widget>[
-
           Padding(
-            padding: const EdgeInsets.only(right:25.0),
+            padding: const EdgeInsets.only(right: 25.0),
             child: IconButton(
-              icon: Icon(
-                Icons.home,
-                color: Colors.white,
-                  size:30.0
-              ),
+              icon: Icon(Icons.home, color: Colors.white, size: 30.0),
               onPressed: () {
-                Get.offAll(()=>Home());
+                Get.offAll(() => Home());
                 // do something
               },
             ),
@@ -97,9 +89,22 @@ class FooterPageState extends State<FooterPage> {
                           height: 300,
                         ),
                       ),
+
                       Padding(
-                        padding: EdgeInsets.only(top: 50, left: 70),
-                        child: Text('關於/版權/參考資料 放此～'),
+                        padding: EdgeInsets.only(left: 10,right:10),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+
+                            Text("聯絡資訊：\n\nEmail : ryan11280@gmail.com \n \n            jean890322@gmail.com\n\nTeL :    0966-603-526 ; 0912-223-223"),
+
+                            SizedBox(height: 20,),
+                            Text('''https://github.com/nyakaz73/Flutter-Footer //MIT //footer \nhttps://github.com/flutter/packages/tree/main/third_party/packages/cupertino_icons //MIT //Cupertino icons\nhttps://pub.dev/packages/firebase_core //firebase core //BSD-3-Clause\nhttps://pub.dev/packages/firebase_storage //firebase storage //BSD-3-Clause\nhttps://pub.dev/packages/google_fonts  // google fonts //Apache 2-.0\nhttps://pub.dev/packages/flutter_svg  //flutter svg //MIT\nhttps://pub.dev/packages/firebase_auth //firebase auth //BSD-3-Clause\nhttps://pub.dev/packages/google_sign_in   // google sign in //BSD-3-Clause\n https://pub.dev/packages/xen_popup_card  //xen popup card // MIT\nhttps://pub.dev/packages/http\n //http // BSD-3-Clause\nhttps://pub.dev/packages/fluttertoast   //flutte troast  //MIT\nhttps://pub.dev/packages/file_picker   //file picker // MIT\nhttps://pub.dev/packages/csv   //csv //MIT
+
+                                '''),
+                          ],
+                        ),
                       )
                     ],
                   ),
@@ -120,7 +125,7 @@ class FooterPageState extends State<FooterPage> {
                           color: Color(0xFF162A49)),
                     ),
                     Text(
-                      'Powered by Question',
+                      'Powered by QA',
                       style: TextStyle(
                           fontWeight: FontWeight.w300,
                           fontSize: 12.0,
